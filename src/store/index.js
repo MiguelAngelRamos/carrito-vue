a406330e4fced6c2df64ca6191741e09e2f6ec2d
Miguel Angel Ramos <return.infox@gmail.com>
2024-05-14 11:19:18 -0400

    vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,25 @@
 import { createStore } from 'vuex';
+import productService from '@/services/api';
 
 export default createStore({
   state: {
-
+    productos: [],
+    carrito: {}
   },
   mutations: {
-
+    setProductos(state, productos) {
+      state.productos = productos;
+    }
   },
   actions: {
-    
-  }
+    async getProductosApi({commit}) {
+      try {
+        const data = await productService.all();
+        console.log(data);
+        commit('setProductos', data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  },
 })
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,6 +5,8 @@
 </template>
 
 <script>
+import { useStore } from 'vuex';
+import { computed, onMounted } from 'vue';
 import Navbar from '@/components/Navbar.vue'
 import Products from '@/components/Products.vue'
 import Layout from '@/layout/Layout.vue'
@@ -14,7 +16,21 @@ export default {
     Navbar,
     Products,
     Layout
+  },
+  setup() {
+    const store = useStore();
+
+    onMounted ( () => {
+      store.dispatch('getProductosApi')
+    });
+
+    const getProductos = computed(() => store.state.productos);
+
+    return {
+      getProductos
+    }
   }
+
 }
 </script>
 
